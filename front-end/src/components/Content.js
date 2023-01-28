213f98521bfec39e3ea9abf133bd2afabdeaf5af
Dorjee <rishav345@gmail.com>
2023-01-28 15:13:05 -0500

    grammatical errors, new build

--- front-end/src/components/Content.js
+++ front-end/src/components/Content.js
@@ -15,17 +15,16 @@ function Content() {
         <Card.Body className="intro-flybird">
           <Card.Text>
             <h1>Hello! Welcome to the flybird app!</h1>
-           You may be wondering what is this box doing here....? Honestly I
-           wonder as well...but I want to show you the bigger picture. Let me
-           ask you what is your screen time? Probably a lot and so is mine!
-           This app is designed for folks like you who spend countless hours
-           scrolling through mindless content, maybe you ask is there a way to
-           just see what you want without having to see irrelevant posts from
-           strangers and famous people. But there is a easier way to browse twitter
-           now and just check the tweets for the users you want. It's that easy
-           you just have to search for the user you want to find and there you
-           go. Why make things harder when they already are, instead lets make
-           it's easier. 𓅨 𓅔 𓅓 𓅊
+            You may be wondering what this box is doing here. Honestly, I wonder
+            as well, but I want to show you the bigger picture. Let me ask you,
+            what is your screen time? Probably a lot, and mine is too! This app
+            is designed for folks like you who spend countless hours scrolling
+            through mindless content. Maybe you're asking, is there a way to
+            just see what you want without having to see irrelevant posts from
+            strangers and famous people? But there is an easier way to browse
+            Twitter now. You just have to search for the user you want to find
+            and there you go. Why make things harder when they already are?
+            Instead, let's make it easier. 𓅨 𓅔 𓅓 𓅊
           </Card.Text>
         </Card.Body>
       </Card>
